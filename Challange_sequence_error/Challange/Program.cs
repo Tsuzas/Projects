1e3f008771a61db7f489fec560edfeb33524e2a3
Tsuzas <f.pereira.2002@hotmail.com>
2024-05-20 14:31:19 +0100

    Projects until 20/5

---
+++ Challange_sequence_error/Challange/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+        int minLength = 4;
+        int maxLength = 4;
+        string[] items = orderStream.Split(',');
+
+        foreach (string item in items)
+        {
+
+            try
+            {
+                ValidateStringLength(item, minLength, minLength);
+                ValidateStringLength(item, minLength, maxLength);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Error in: {item}");
+            }
+            static void ValidateStringLength(string str, int minLength, int maxLength)
+            {
+                if (str.Length < minLength)
+                {
+                    throw new ArgumentException($"The string must be at least {minLength} characters long.");
+                }
+                if (str.Length > maxLength)
+                {
+                    throw new ArgumentException($"The string must be at least {maxLength} characters long.");
+                }
+            }
+            Console.WriteLine(item);
+        }
+    }
+}
+
---
+++ Formatting_Stuff/Fortmatting Stuff/Program.cs
@@ -0,0 +1,29 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+//------------------------------------
+int openingPosition = input.IndexOf("<span>");
+int closingPosition = input.IndexOf("</span>");
+
+openingPosition += 6;
+
+int length = closingPosition - openingPosition;
+
+output = input;
+output = output.Remove(0, "<div>".Length);
+
+int position = output.IndexOf("</div>");
+Console.WriteLine(position);
+
+output = output.Remove(position, "</div>".Length);
+
+output = output.Replace("&trade;", "&reg;");
+//-------------------------------------
+Console.WriteLine($"Quantity: {input.Substring(openingPosition, length)}");
+Console.WriteLine($"Output: {output}");
+
+
+
+
+
